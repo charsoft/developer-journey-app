dfa2a1cb452167c161c0cc9a0c7c2db93e1a3866
Charlene <charlene@charsoft.com>
2025-03-28 12:32:58 -0400

    more

--- src/components/mission-history.tsx
+++ src/components/mission-history.tsx
@@ -34,12 +34,12 @@ export default function MissionHistory() {
         <div className="relative overflow-x-auto">
           {isLoading ? ('Loading...') : (<>
             <div className='mb-2'>
-              {user.completedMissions.filter(missionId => missions.find(mission => mission.id === missionId)).length}
+              {(user?.completedMissions || []).filter(missionId => missions.find(mission => mission.id === missionId)).length}
               {' '}
               Completed Missions
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-fit">
-              {user.completedMissions.map((missionId, index) => {
+              {(user?.completedMissions || []).map((missionId, index) => {
                 const mission = missions.find(mission => mission.id === missionId);
                 if (!mission) return null;
                 return (
